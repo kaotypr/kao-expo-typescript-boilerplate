aabfc48c7db977184116bfa8c633b14a55464bb6
adhitya yosua sanjaya <adityasanjaya@sicepat.com>
2022-07-26 03:48:47 +0700

    docs: ✏️ how to create a new navigation

--- src/@types/navigations/root.d.ts
+++ src/@types/navigations/root.d.ts
@@ -1,6 +1,6 @@
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-import { SCREEN_NAMES } from "../../services/constants/screen";
+import { SCREEN_NAMES } from "@services/constants/screen";
 
 type RootStackParamList = {
   [SCREEN_NAMES.HOME]: undefined;
